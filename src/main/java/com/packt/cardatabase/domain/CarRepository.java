783f9a51e15d071557daef26b61140dd54119b61
Rajesh Kumar Kanaparthi <rajeshkumar.kanaparthi@outlook.com>
2023-01-17 22:11:57 -0600

    ch2

---
+++ src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -0,0 +1,26 @@
+package com.packt.cardatabase.domain;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+
+	List<Car> findByBrand(String brand);
+
+	List<Car> findByColor(String color);
+
+	List<Car> findByYear(int year);
+	
+	List<Car> findByBrandAndModel(String brand, String model);
+
+	List<Car> findByBrandOrColor(String brand, String color);
+	
+	List<Car> findByBrandOrderByYearAsc(String brand);
+	
+	//@Query(“select c from Car c where c.brand = ?1")
+	//List<Car> findByBrand(String brand);
+
+
+}
---
+++ src/main/java/com/packt/cardatabase/domain/Owner.java
@@ -0,0 +1,53 @@
+package com.packt.cardatabase.domain;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Owner {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long ownerId;
+	private String firstName, lastName;
+	
+	@OneToMany(cascade = CascadeType.ALL,mappedBy = "owner")
+	private List<Car> cars;
+	
+	public Owner() {
+		
+	}
+	public Owner(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public long getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(long ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+	
+	
+
+}
